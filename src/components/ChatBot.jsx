bbecd1e3ddcf213ba7ea8c8d33d65ba743da38d0
surajsinghsft <surajsinghsft@gmail.com>
2026-09-18 18:03:09 +0530

    change url

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -43,7 +43,7 @@ function ChatBot() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/chat", {
+      const response = await fetch("https://portfolio-kxdh.vercel.app/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
